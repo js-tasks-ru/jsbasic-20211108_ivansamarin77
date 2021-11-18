ca5b770b432023bde6736aa7f33b6cf65083bfd1
isamarin <isamarin@ozon.ru>
2021-11-18 10:12:28 +0500

    Added sum function

--- 2-module/1-task/index.js
+++ 2-module/1-task/index.js
@@ -1,3 +1,11 @@
 function sumSalary(salaries) {
-  // ваш код...
+  let sum = 0
+  for (let key in salaries) {
+    if (salaries[key] == NaN || !isFinite(salaries[key])) {
+      break
+    } else {
+      sum += salaries[key]
+    }
+  }
+  return sum
 }
