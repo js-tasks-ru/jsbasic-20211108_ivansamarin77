beec6c5d47850a90f1995571c7b244d827c5bf3d
Иван Самарин <ivansamarin77@mail.ru>
2021-12-06 23:14:48 +0300

    Added carousel function

--- 5-module/3-task/index.js
+++ 5-module/3-task/index.js
@@ -1,3 +1,46 @@
 function initCarousel() {
-  // ваш код...
+  let sliderPosition = 0;
+  const rightButton = document.querySelector('.carousel__arrow_right');
+  const leftButton = document.querySelector('.carousel__arrow_left');
+  const slider = document.querySelector('.carousel__inner');
+  const sliderWidth = slider.clientWidth
+
+  if (sliderPosition === 0) {
+    leftButton.style.display = 'none'
+  } 
+
+  leftButton.addEventListener('click', () => {
+    sliderPosition += sliderWidth
+    
+    if (sliderPosition > 0) {
+      sliderPosition = 0;
+    }
+
+    if (sliderPosition === 0) {
+      leftButton.style.display = 'none'
+    }
+
+    if (sliderPosition > -2963) {
+      rightButton.style.display = ''
+    }
+
+    slider.style.transform = `translateX(${sliderPosition}px)`
+  })
+
+  rightButton.addEventListener('click', () => {
+    sliderPosition -= sliderWidth
+
+    if (sliderPosition < -2964) {
+      sliderPosition = -2964;
+    }
+    
+    if (sliderPosition === -2964) {
+      rightButton.style.display = 'none'
+    } 
+    
+    if (sliderPosition < 0) {
+      leftButton.style.display = ''
+    }
+    slider.style.transform = `translateX(${sliderPosition}px)`
+  })
 }
